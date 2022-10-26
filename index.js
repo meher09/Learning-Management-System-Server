4a7ebc6a39abb98b229bee10f64262329ed7e46c
meher09 <mehernigarcu@gmail.com>
2022-10-26 15:55:42 +0600

    course intro data primary added

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/course-categories.json');
-const news = require('./data/news.json');
+const courses = require('./data/course-intro.json');
+
 
 app.get('/course-categories', (req, res) => {
     res.send(categories)
 });
 
 
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const course_category = courses.filter(n => n.category === id);
+        res.send(course_category);
+    }
+})
+
+
+app.listen(port, () => { })
